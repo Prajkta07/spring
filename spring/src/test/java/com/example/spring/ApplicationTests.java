375e596c04ab167a5709a8ffb8d9852be6e5ba78
vids008 <vidswaghmare@gmail.com>
2019-06-15 09:22:15 +0530

    first commit

---
+++ spring/src/test/java/com/example/spring/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.spring;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
